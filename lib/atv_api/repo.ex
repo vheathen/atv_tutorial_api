6201e8014aae5f53ceba74ea50b680165347d4ca
heathen <v.heathen@gmail.com>
2017-02-12 00:14:43 +0500

    Initial commit

---
+++ lib/atv_api/repo.ex
@@ -0,0 +1,3 @@
+defmodule AtvApi.Repo do
+  use Ecto.Repo, otp_app: :atv_api
+end
---
+++ web/router.ex
@@ -0,0 +1,11 @@
+defmodule AtvApi.Router do
+  use AtvApi.Web, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", AtvApi do
+    pipe_through :api
+  end
+end
